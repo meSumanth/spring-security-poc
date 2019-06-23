b4ffca1b8f23d79a21a9489e8f490177a40e7747
Sumanth <va20070770@wipro.com>
2019-06-23 12:47:06 +0530

    Initial Commit

---
+++ src/test/java/com/springsec/poc/springsecutirydemo/SpringSecutiryDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springsec.poc.springsecutirydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecutiryDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
